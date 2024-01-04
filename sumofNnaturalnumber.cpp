244a8223b07dfc332d22c083808cb1530f8d3def
Rohan Sakhare <126242412+rohsak5649@users.noreply.github.com>
2024-01-04 12:42:14 +0530

    Create sumofNnaturalnumber.cpp

---
+++ sumofNnaturalnumber.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std ;
+int main()
+{
+    int a ,i,b;
+    b= 0;
+    cout<<"enter the munber";
+    cin >>a;
+    
+    for (i=1; i<=a; i++ )
+    {
+        b = b+i;
+    }
+    cout<<b;
+    
+    
+    
+    
+    
+}
